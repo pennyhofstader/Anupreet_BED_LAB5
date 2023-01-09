29de34cb74058a8ac438febd1da6cebb0bb32520
Khushi <anupreet.bedi12@gmail.com>
2023-01-09 19:46:05 +0530

    newchanges

--- src/main/java/com/greatlearning/ems/entity/Employee.java
+++ src/main/java/com/greatlearning/ems/entity/Employee.java
@@ -12,9 +12,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 //annotations to generate getters and setters and creating table in database
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
 @Entity
 @Table(name = "Employees")
 public class Employee {
@@ -29,5 +29,34 @@ public class Employee {
 	private String lastName;
 	@Column(name = "email")
 	private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+	
 
 }
